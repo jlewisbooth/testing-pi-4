d54af157f5f5be3458862f2658bc913e86ce419e
Jack Booth <ub00026@MacBook-Pro-2.local>
2022-06-06 14:33:18 +0100

    update

--- communications-server/managers/udp-server.js
+++ communications-server/managers/udp-server.js
@@ -1,6 +1,11 @@
 const dgram = require("dgram");
 const config = require("../config.json");
 const EventEmitter = require("events");
+const redis = require("redis");
+
+function getRedisClient() {
+  return redis.createClient();
+}
 
 // create datagram server
 const server = dgram.createSocket("udp4");
@@ -13,16 +18,48 @@ class UDPServer extends EventEmitter {
     this.server = server;
     this.subscribers = [];
 
-    this.server.on("message", (msg, rinfo) => {
-      let packet = JSON.parse(msg);
-      console.log("Recieved a datagram packet:", packet);
-    });
+    this.server.on("message", this._handleGramPacket.bind(this));
+
+    this.initiateClient();
+
+    this.publishChannel = config.tags.fromSensor + "ub.model-uk.tower-bridge";
+  }
+
+  async initiateClient() {
+    await this._ensureRedisClient();
+  }
+
+  async _ensureRedisClient() {
+    if (!this.redisPublisher) {
+      this.redisPublisher = getRedisClient();
+
+      await this.redisPublisher.connect();
+    }
+  }
+
+  _handleGramPacket(msg) {
+    // only expecting packets from ub.model-uk.tower-bridge
+    // msg not parsed for now
+
+    this.redisPublisher.publish(this.publishChannel, msg);
   }
 
   sendPacket(packet) {
     let msg = JSON.stringify(packet);
     for (let sub of this.subscribers) {
       this.server.send(msg, 0, msg.length, sub.port, sub.address);
+    }
+  }
+
+  close() {
+    if (this.redisClient) {
+      this.redisClient.unsubscribe();
+      this.redisClient.quit();
+    }
+
+    if (this.redisPublisher) {
+      this.redisPublisher.unsubscribe();
+      this.redisPublisher.quit();
     }
   }
 }
