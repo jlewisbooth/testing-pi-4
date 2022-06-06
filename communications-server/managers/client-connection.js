d2b8eb3578efcb8cb5f8ccb695311b15044fcefe
Jack Booth <ub00026@MacBook-Pro-2.local>
2022-06-06 14:00:20 +0100

    update

--- communications-server/managers/client-connection.js
+++ communications-server/managers/client-connection.js
@@ -66,7 +66,7 @@ class ClientConnection extends EventEmitter {
       },
     });
 
-    console.log(packet);
+    console.log(JSON.parse(packet));
 
     this.emit("message", {
       locationId: channel,
